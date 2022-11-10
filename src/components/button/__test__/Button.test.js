7136c6b13704983bcf4ab530b353138e4dc01274
Xue Yu <xueyublue@gmail.com>
2022-11-10 21:34:29 +0800

    Added react-test-renderer dependency to snapshot test case

--- src/components/button/__test__/Button.test.js
+++ src/components/button/__test__/Button.test.js
@@ -1,10 +1,16 @@
 import { render, screen, cleanup } from "@testing-library/react";
 import Button from "../Button";
+import renderer from "react-test-renderer";
 
 beforeEach(cleanup);
 
-test("renders", () => {
+test("render success", () => {
   render(<Button label="SAVE"></Button>);
   const button = screen.getByTestId("button");
   expect(button).toHaveTextContent("SAVE");
+});
+
+test("render snapshot", () => {
+  const tree = renderer.create(<Button label="SAVE" />).toJSON();
+  expect(tree).toMatchSnapshot();
 });

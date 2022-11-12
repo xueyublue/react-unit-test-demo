a2075ca8798eeb525a168761e2c3bed235ed6449
Xue Yu <xueyublue@gmail.com>
2022-11-12 18:55:52 +0800

    Fixed test failed issue

--- src/_02_sundaes/__test__/Options.test.js
+++ src/_02_sundaes/__test__/Options.test.js
@@ -6,7 +6,7 @@ describe("basic tests", () => {
     render(<Options optionType="scoops" />);
 
     const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
-    expect(scoopImages).toHaveLength(4);
+    expect(scoopImages).toHaveLength(2);
 
     const altText = scoopImages.map((element) => element.alt);
     expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);

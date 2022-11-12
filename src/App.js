13307e690069e23d7f54756b2687f2f118e18eb5
Xue Yu <xueyublue@gmail.com>
2022-11-12 14:50:51 +0800

    Added mouse hover/unhover test cases

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import SimpleButtonApp from "./_01_simple_button/SimpleButtonApp";
+import SummaryForm from "./_02_sundaes/SummaryForm";
 
 function App() {
   return (
     <div>
-      <SimpleButtonApp />
+      {/* <SimpleButtonApp /> */}
+      <SummaryForm />
     </div>
   );
 }

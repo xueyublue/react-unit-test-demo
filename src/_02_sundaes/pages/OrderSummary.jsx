290e79621f764080586efce155bec6c5120adcc7
Xue Yu <xueyublue@gmail.com>
2022-11-17 16:13:04 +0800

    Moved to pages folder

---
+++ src/_02_sundaes/pages/OrderSummary.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useOrderDetails } from "../context/OrderDetails";
+import { formatCurrency } from "../utils/index";
+import SummaryForm from "./SummaryForm";
+
+export default function OrderSummary() {
+  const { totals, optionCounts } = useOrderDetails();
+  const scoopArray = Object.entries(optionCounts.scoops);
+  const scoopList = scoopArray.map(([key, value]) => (
+    <li key={key}>
+      {value} {key}
+    </li>
+  ));
+  const toppingsArray = Object.entries(optionCounts.toppings);
+  const toppingsList = toppingsArray.map(([key, value]) => (
+    <li key={key}>
+      {value} {key}
+    </li>
+  ));
+
+  return (
+    <div>
+      <h1>Order Summary</h1>
+      <h2>Scoops: {formatCurrency(totals.scoops)}</h2>
+      <ul>{scoopList}</ul>
+      <h2>Toppings: {formatCurrency(totals.toppings)}</h2>
+      <ul>{toppingsList}</ul>
+      <SummaryForm />
+    </div>
+  );
+}

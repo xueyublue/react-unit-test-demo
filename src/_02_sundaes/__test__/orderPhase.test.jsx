e142684cb50365a69bdca26de00594950b786a07
Xue Yu <xueyublue@gmail.com>
2022-11-17 16:22:49 +0800

    commit first

---
+++ src/_02_sundaes/__test__/orderPhase.test.jsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "../../App";
+
+describe("order phases test", () => {
+  test("happy flow", () => {
+    render(<App />);
+
+    // add ice cream scoops and toppings
+
+    // find and click order button
+
+    // check summary information based on order
+
+    // accept terms and conditions and click button to confirm order
+  });
+});

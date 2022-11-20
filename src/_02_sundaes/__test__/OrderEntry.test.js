fd4ac00069c5dd549b6048ef20c5bc0380693fea
Xue Yu <xueyublue@gmail.com>
2022-11-20 11:00:40 +0800

    add jest mock

--- src/_02_sundaes/__test__/OrderEntry.test.js
+++ src/_02_sundaes/__test__/OrderEntry.test.js
@@ -9,7 +9,7 @@ describe("test", () => {
       rest.get("http://localhost:3030/scoops", (req, res, ctx) => res(ctx.status(500))),
       rest.get("http://localhost:3030/toppings", (req, res, ctx) => res(ctx.status(500)))
     );
-    render(<OrderEntry />);
+    render(<OrderEntry setOrderPhase={jest.fn()} />);
 
     await waitFor(async () => {
       const alerts = await screen.findAllByRole("alert");
